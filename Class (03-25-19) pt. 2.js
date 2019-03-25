02d3e54325d16b5eb6803043bd06265a3d740238
TorieWIN <48681532+TorieWIN@users.noreply.github.com>
2019-03-25 14:25:26 -0400

    Add files via upload

---
+++ Class (03-25-19) pt. 2.js
@@ -0,0 +1,9 @@
+var myVar = "b"
+switch(myVar) {
+    case "a": console.log("Hey");
+    break;
+    case "b": console.log("Foo")
+    case "b": console.log("Bar")
+    break;
+    case "c": console.log("Bar");
+}
